0f21ee2d56efaba38ca6cc8c803268b1b6d920bd
Manuel <35193759+mancaslun@users.noreply.github.com>
2018-01-07 15:18:30 +0100

    Prueba

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -3,16 +3,22 @@ import logo from '../assets/logo.svg';
 import '../styles/screens/home.css';
 
 class Home extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {name: 'Manolo'}
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/screens/Home.js</code> and save to reload.
         </p>
+        <p>Holka mund {this.state.name}!!</p>
       </div>
     );
   }
